003b1a1c92500f9134326be4cfde8b197ecbb08d
Carlos Antonio <carlosantonioob@gmail.com>
2024-12-15 16:29:06 -0300

    Create ContadorFor.java

---
+++ ContadorFor.java
@@ -0,0 +1,21 @@
+package CursoGuranabara;
+//contador em java
+
+import java.util.*;
+public class ContadorFor {
+    public static void main(String[] agrs){
+        int inicio, fim, passo;
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite o inicio do contador: ");
+        inicio = teclado.nextInt();
+        System.out.print("Digite o final do conrtador: ");
+        fim = teclado.nextInt();
+        System.out.print("Digite de quanto sera o passo: ");
+        passo = teclado.nextInt();
+        for(int i = inicio; i <= fim; i += passo ){
+            System.out.print(i +" ");
+        }
+
+
+    }
+}
