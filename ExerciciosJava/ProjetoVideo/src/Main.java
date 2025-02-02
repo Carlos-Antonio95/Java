86fff8185abf8072de19223a6061fe324608fcc6
Carlos Antonio <carlosantonioob@gmail.com>
2025-02-02 16:24:41 -0300

    Foi criada a classe visualização

--- ExerciciosJava/ProjetoVideo/src/Main.java
+++ ExerciciosJava/ProjetoVideo/src/Main.java
@@ -8,9 +8,13 @@ public class Main {
         Gafanhoto g[] = new Gafanhoto[2];
         g[0] = new Gafanhoto("Carlos", 28, "Masculino", "lota");
         g[1] = new Gafanhoto("Creuza", 50, "Feminino", "cz");
-        System.out.println(v[0].toString());
-        v[0].play();
-        v[0].pause();
-        System.out.println(g[0].toString());
+
+        Visualizacao vis[] = new Visualizacao[3];
+        vis[0] = new Visualizacao(g[0], v[0]);
+        vis[0] = new Visualizacao(g[0], v[1]);
+        vis[0] = new Visualizacao(g[1], v[2]);
+        vis[0] = new Visualizacao(g[1], v[0]);
+       
+        
     }
 }
